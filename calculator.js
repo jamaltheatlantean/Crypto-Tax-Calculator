47b35d6d26eba6981dda14a1d2cb807d99c89fb4
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-03-25 16:02:51 +0100

    push modified code

--- calculator.js
+++ calculator.js
@@ -9,12 +9,21 @@ form.addEventListener('submit', async (event) => {
   const amountUsd = document.querySelector('#amount-usd').value;
   const sold = document.querySelector('#sold').value;
   const soldUsd = document.querySelector('#sold-usd').value;
-  const date = document.querySelector('#date').value;
+  const purchaseDate = document.querySelector('#purchase-date').value;
+  const saleDate = document.querySelector('#sale-date').value;
   
   // Get the price of the crypto on the purchase date
-  const query = `
+  const purchaseQuery = `
     {
-      ${crypto}(date: {eq: "${date}"}) {
+      ${crypto}(date: {eq: "${purchaseDate}"}) {
+        price
+      }
+    }
+  `;
+  
+  const saleQuery = `
+    {
+      ${crypto}(date: {eq: "${saleDate}"}) {
         price
       }
     }
@@ -22,28 +31,40 @@ form.addEventListener('submit', async (event) => {
 
   const url = `https://graphql.bitquery.io/`;
 
-  const response = await fetch(url, {
+  const purchaseResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'BITQUERY-API-KEY': 'YOUR_API_KEY' // Will replace with actual API key once i get it
+    },
+    body: JSON.stringify({ query: purchaseQuery })
+  });
+
+  const saleResponse = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'X-API-KEY': 'YOUR_API_KEY' // Replace with your actual API key
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({ query: saleQuery })
   });
 
-  const data = await response.json();
+  const purchaseData = await purchaseResponse.json();
+  const saleData = await saleResponse.json();
 
-  const price = data.data[crypto][0].price;
+  const purchasePrice = purchaseData.data[crypto][0].price;
+  const salePrice = saleData.data[crypto][0].price;
 
   // Calculate the tax
   const purchaseValue = amountUsd;
-  const soldValue = soldUsd;
-  const capitalGain = soldValue - purchaseValue;
+  const saleValue = soldUsd;
+  const capitalGain = saleValue - purchaseValue;
   const taxRate = 0.15; // Replace with your actual tax rate
   const taxOwed = capitalGain * taxRate;
 
   // Display the result
-  resultDiv.textContent = `You owe $${taxOwed.toFixed(2)} in taxes on your ${crypto} purchase.`;
+  resultDiv.textContent = `You owe $${taxOwed.toFixed(2)} in taxes on your ${crypto} transaction (purchased on ${purchaseDate} and sold on ${saleDate}).`;
+
 });
 
 // Retrieve the user's language preference and user agent string
