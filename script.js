44d21bf9187c977b01a373a82e7a0d19d164d415
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-03-29 18:02:21 +0100

    modify javascript

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const form = document.querySelector('form');
-const resultDiv = document.querySelector('#result');
+const form = document.querySelector("form");
+const resultDiv = document.querySelector("#result");
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const crypto = document.querySelector('#crypto').value;
-  const amount = document.querySelector('#amount').value;
-  const amountUsd = document.querySelector('#amount-usd').value;
-  const sold = document.querySelector('#sold').value;
-  const soldUsd = document.querySelector('#sold-usd').value;
-  const purchaseDate = document.querySelector('#purchase-date').value;
-  const saleDate = document.querySelector('#sale-date').value;
-  
+  const crypto = document.querySelector("#crypto").value;
+  const amountUsd = document.querySelector("#amount-usd").value;
+  const soldUsd = document.querySelector("#sold-usd").value;
+  const purchaseDate = document.querySelector("#purchaseDate").value;
+  const saleDate = document.querySelector("#saleDate").value;
+
   // Get the price of the crypto on the purchase date
   const purchaseQuery = `
     {
@@ -20,7 +18,7 @@ form.addEventListener('submit', async (event) => {
       }
     }
   `;
-  
+
   const saleQuery = `
     {
       ${crypto}(date: {eq: "${saleDate}"}) {
@@ -32,28 +30,28 @@ form.addEventListener('submit', async (event) => {
   const url = `https://graphql.bitquery.io/`;
 
   const purchaseResponse = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'BITQUERY-API-KEY': 'YOUR_API_KEY' // Will replace with actual API key once i get it
+      "Content-Type": "application/json",
+      "X-API-KEY": "BQYgbliUSjLJlBzRVKqe0HNY4vtybLGt", // Will replace with actual API key once i get it
     },
-    body: JSON.stringify({ query: purchaseQuery })
+    body: JSON.stringify({ query: purchaseQuery }),
   });
 
   const saleResponse = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': 'YOUR_API_KEY' // Replace with your actual API key
+      "Content-Type": "application/json",
+      "X-API-KEY": "BQYgbliUSjLJlBzRVKqe0HNY4vtybLGt", // Replace with your actual API key
     },
-    body: JSON.stringify({ query: saleQuery })
+    body: JSON.stringify({ query: saleQuery }),
   });
 
   const purchaseData = await purchaseResponse.json();
   const saleData = await saleResponse.json();
 
-  const purchasePrice = purchaseData.data[crypto][0].price;
-  const salePrice = saleData.data[crypto][0].price;
+  //const purchasePrice = purchaseData.data[crypto][0].price;
+  //const salePrice = saleData.data[crypto][0].price;
 
   // Calculate the tax
   const purchaseValue = amountUsd;
@@ -63,8 +61,9 @@ form.addEventListener('submit', async (event) => {
   const taxOwed = capitalGain * taxRate;
 
   // Display the result
-  resultDiv.textContent = `You owe $${taxOwed.toFixed(2)} in taxes on your ${crypto} transaction (purchased on ${purchaseDate} and sold on ${saleDate}).`;
-
+  resultDiv.textContent = `You owe $${taxOwed.toFixed(
+    2
+  )} in taxes on your ${crypto} transaction (purchased on ${purchaseDate} and sold on ${saleDate}).`;
 });
 
 // Retrieve the user's language preference and user agent string
